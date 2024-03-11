530f0582e77ca617a1e84ba6434086f0114af4c0
Abinaya1433 <sivaabi1433@gmail.com>
2024-03-11 04:06:55 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://mir-s3-cdn-cf.behance.net/projects/404/9a2c4a124322543.61018375e88c5.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://blog-application-backend-three.vercel.app"
